56dc4277463908bb3b478e699cbc136bda69c936
ashwinii <ashwinipowar2111@gmail.com>
2026-03-16 18:07:43 +0530

    Add files via upload

---
+++ tuple method.py
@@ -0,0 +1,23 @@
+# count 
+# tuple1 = (0,2,3,4,5,6,6,7,7,7,7,7,4)
+# res = tuple1.count(7)
+# print('count  of 7 in the tuple:', res)
+
+
+# index
+# tuple1 = (0,2,3,4,5,6,6,7,7,7,7,7,4)
+# res = tuple1.index(2)
+# print('index  of 2 in the tuple:', res)
+
+# when wants indexing from perticilar element to perticular element
+# tuple1 = (0,1,2,3,2,3,1,3,2,3)
+# res = tuple1.index(3, 4, 8) # want which number this should be in starting
+# print('count of 3 in tuple is:', res)
+# giving ans 5 bcz its in the position of 5 when at perticular range
+
+
+# length
+tuple1 = (0,1,2,3,2,3,1,3,2,3)
+res = len(tuple1)
+print('count of 3 in tuple is:', res)
+# giving the ans is 10 which is its length
